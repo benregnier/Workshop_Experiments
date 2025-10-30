8e7eff60206aa28352bb6e55bf1bc01fb3c67796
Benjamin Regnier <104884034+benregnier@users.noreply.github.com>
2025-10-30 08:59:33 -0700

    Update main.cpp
    extended template with documentation and structure

--- Vink/main.cpp
+++ Vink/main.cpp
@@ -1,11 +1,43 @@
 #include "ComputerCard.h"
 
-/// Vink
-/// Dual delay loops + ring mod with a limiter for Jaap Vink / Roland Kayn style feedback patching
-/// 
+/* Vink
+	Dual delay loops + ring mod with a limiter for Jaap Vink / Roland Kayn style feedback patching 
+	For more info see this Mr Sonology Video: https://www.youtube.com/watch?v=X_Bcr_HS9XM
+
+	Blocks:
+		- Two delay taps (x to y ms)
+		- Ring modulators for each loop sharing a single input
+		- Compressor/limiter end of chain
+
+	Control Mapping:
+		Knob::Main: shared delay timing control (becomes attenuator for CVIn1 when patched)
+		Knob::X: delay timing spread (becomes attenuator for CVIn2 when patched)
+		Knob::Y: limiter volume
+
+		AudioIn1: Audio In
+		AudioIn2: Ring Modulator In (disconnects when not patched)
+		CVIn1: Main Knob control
+		CVIn2: Knob X control
+		Switch: Select split (Up) or shared (mom/down) output
+		AudioOut1: Tap 1 or shared
+		AudioOut2: Tap 2 if split
+		(Optional) CVOut1 envelope follower signal from limiter?
+		
+*/
+
+// Helper Functions
+
+// Vink State
+struct VinkState {
+};
+
 class Vink : public ComputerCard
 {
 public:
+	VinkState vink;
+
+	Vink(){
+	};
 	
 	virtual void ProcessSample()
 	{
