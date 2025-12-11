893dd0e5c0fa5b05d6ac746d378b9a66b8e699c7
Benjamin Regnier <104884034+benregnier@users.noreply.github.com>
2025-12-10 19:30:15 -0800

    Add Mellotron emulator main.cpp file
    Initial commit of the Mellotron emulator with comments.

---
+++ Mello/main.cpp
@@ -0,0 +1,9 @@
+// Mellotron emulator
+// Load in your own sounds! [sample spec here] include on the card at load.
+// - Main knob demos various available samples
+// - Two channels
+// - Audio In: amplitude
+// - CV In: pitch
+// - Trigger in: TBD
+// - X and Y are vibrato and tremolo
+// - Switch up or momentary: drone mode
