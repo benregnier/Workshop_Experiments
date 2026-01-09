f91319201c3f27cebbcd7c6d4bc413b15287bb76
Benjamin Regnier <104884034+benregnier@users.noreply.github.com>
2026-01-08 16:35:27 -0800

    Refactor quantization and fix sample processing

--- AltQuant/main.cpp
+++ AltQuant/main.cpp
@@ -1,22 +1,56 @@
 #include "ComputerCard.h"
+#include <stdint.h>
 
 // Quantizer that provides alternative 12TET tunings
 // LOG(LOG(4 + 12 * $E12, 2), 2) - 1
+
+
+int32_t log2_note[12] = {0, 215, 370, 489, 585, 664, 732, 791, 842, 888, 929, 966}; // log2 values for 12TET in millivolts
+
+struct QuantizedNote {
+  uint8_t octave;   // 0..(octaves-1)
+  uint8_t note;     // 0..(notesPerOctave-1)
+  uint16_t step;    // 0..(octaves*notesPerOctave-1)
+};
+
+// Maps adc in [0..4091] to step in [0..totalSteps-1] with no overflow and
+// ensures adc=4091 -> step=totalSteps-1.
+static inline QuantizedNote Quantize_12v(uint16_t adc,
+                                            uint8_t octaves = 12,
+                                            uint8_t notesPerOctave = 12)
+{
+  // Clamp just in case
+  if (adc > 4091) adc = 4091;
+
+  const uint16_t totalSteps = (uint16_t)octaves * (uint16_t)notesPerOctave; // e.g. 144
+  // Use 4092 as the span so the top code lands on the last step.
+  const uint16_t step = (uint16_t)(((uint32_t)adc * totalSteps) / 4092u);
+
+  QuantizedNote q;
+  q.step   = step;
+  q.octave = (uint8_t)(step / notesPerOctave);
+  q.note   = (uint8_t)(step % notesPerOctave);
+  return q;
+}
+
+	
 
 class AltQuant : public ComputerCard
 {
 public:
 
-  int32_t log2[] = {0, 215, 370, 489, 585, 664, 732, 791, 842, 888, 929, 966}; // log2 values for 12TET in millivolts
+  
 
 	virtual void ProcessSample()
 	{
-    int32_t qin1 = CVIn1()
-    int32_t qin2 = CVIn2()
+	    int32_t qin1 = CVIn1();
+	    int32_t qin2 = CVIn2();
+	
+	    QuantizedNote q1 = Quantize_12v(qin1);
+		QuantizedNote q1 = Quantize_12v(qin2);
 
-    //TODO:convert qin1 and 2 to voltages
-    //TODO:separate octave and quantize remainder to 12TET
-    //TODO: get log2 mv and combine with octave, write to cv1mv and cv2mv
+		int32_t cm1mv = q1.octave * 1000 + log2_note[q1.note];
+		int32_t cm2mv = q2.octave * 1000 + log2_note[q2.note];
     
 		// Output desired voltages on CV out
 		bool cv1limited = CVOut1Millivolts(cv1mv);
